3c16d22516be57a106814f049c158e93a120d81f
mshems <mshems@u.rochester.edu>
2017-11-21 09:47:14 -0500

    update

--- RelationalDataModel/rdm.c
+++ RelationalDataModel/rdm.c
@@ -1,116 +1,25 @@
-#include "rdm.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-CSG_RELATION csg_table[6];
-SNAP_RELATION snap_table[3];
-CPQ_RELATION cpq_table[8];
-CDH_RELATION cdh_table[6];
-CR_RELATION cr_table[3];
+#include "rdm.h"
 
-CSG_RELATION new_CSG(char* c, int s, char* g){
-  CSG_RELATION new_csg = (CSG_RELATION)malloc(sizeof(struct _CSG));
-  new_csg->course_name = c;
-  new_csg->student_id = s;
-  new_csg->student_grade = g;
-  return new_csg;
-}
-
-SNAP_RELATION new_SNAP(int s, char* n, char* a, int p){
-  SNAP_RELATION new_snap = (SNAP_RELATION)malloc(sizeof(struct _SNAP));
-  new_snap->student_id = s;
-  new_snap->student_name = n;
-  new_snap->student_address = a;
-  new_snap->student_phone = p;
-  return new_snap;
-}
-
-CPQ_RELATION new_CPQ(char* c, char* pq){
-  CPQ_RELATION new_cpq = (CPQ_RELATION)malloc(sizeof(struct _CPQ));
-  new_cpq->course_name = c;
-  new_cpq->prereq_name = pq;
-  return new_cpq;
-}
-
-CDH_RELATION new_CDH(char* c, char* d, char* h){
-  CDH_RELATION new_cdh = (CDH_RELATION)malloc(sizeof(struct _CDH));
-  new_cdh->course_name = c;
-  new_cdh->course_day = d;
-  new_cdh->course_hour = h;
-  return new_cdh;
-}
-
-CR_RELATION new_CR(char* c, char* r){
-  CR_RELATION new_cr = (CR_RELATION)malloc(sizeof(struct _CR));
-  new_cr->course_name = c;
-  new_cr->course_room =r;
-  return new_cr;
-}
-
-void create_tables(){
-  csg_table[0] = new_CSG("CS101", 12345, "A");
-  csg_table[1] = new_CSG("CS101", 67890, "B");
-  csg_table[2] = new_CSG("EE200", 12345, "C");
-  csg_table[3] = new_CSG("CS101", 22222, "B+");
-  csg_table[4] = new_CSG("CS101", 33333, "A-");
-  csg_table[5] = new_CSG("PH100", 12345, "C+");
-
-  snap_table[0] = new_SNAP(12345, "C. Brown", "12 Apple St.", 5551234);
-  snap_table[1] = new_SNAP(67890, "L. Van Pelt", "34 Pear Ave.", 5555678);
-  snap_table[2] = new_SNAP(22222, "P. Patty", "56 Grape Blvd.", 5559999);
-
-  cpq_table[0] = new_CPQ("CS101", "CS100");
-  cpq_table[1] = new_CPQ("EE200", "EE005");
-  cpq_table[2] = new_CPQ("EE200", "CS100");
-  cpq_table[3] = new_CPQ("CS120", "CS101");
-  cpq_table[4] = new_CPQ("CS121", "CS120");
-  cpq_table[5] = new_CPQ("CS205", "CS101");
-  cpq_table[6] = new_CPQ("CS206", "CS121");
-  cpq_table[7] = new_CPQ("CS206", "CS205");
-
-  cdh_table[0] = new_CDH("CS101", "M", "9AM");
-  cdh_table[1] = new_CDH("CS101", "W", "9AM");
-  cdh_table[2] = new_CDH("CS101", "F", "9AM");
-  cdh_table[3] = new_CDH("EE200", "Tu", "10AM");
-  cdh_table[4] = new_CDH("EE200", "W", "1PM");
-  cdh_table[5] = new_CDH("EE200", "Th", "10AM");
-
-  cr_table[0] = new_CR("CS101", "Turing Aud.");
-  cr_table[1] = new_CR("EE200", "25 Ohm Hall");
-  cr_table[2] = new_CR("PH100", "Newton Lab.");
-}
-
-int main(int argc, char const *argv[]) {
-  create_tables();
-  const char* flag = "";
-  if(argv[1] != NULL){
-    flag = argv[1];
+void insert(LinkedList* list, void* relation){
+  if(LinkedList_contains(list, relation) == true){
+    printf(" -- Duplicate Entry -- ");
+  } else {
+    LinkedList_add_at_end(list, relation);
   }
-  if(strcmp(flag, "csg")==0 || strcmp(flag, "")==0){
-    for(int i=0; i<6; i++){
-      print_CSG(csg_table[i]);
-    }
-  }
-  if(strcmp(flag, "snap")==0 || strcmp(flag, "")==0){
-    for(int i=0; i<3; i++){
-      print_SNAP(snap_table[i]);
-    }
-  }
-  if(strcmp(flag, "cpq")==0 || strcmp(flag, "")==0){
-    for(int i=0; i<8; i++){
-      print_CPQ(cpq_table[i]);
-    }
-  }
-  if(strcmp(flag, "cdh")==0 || strcmp(flag, "")==0){
-    for(int i=0; i<6; i++){
-      print_CDH(cdh_table[i]);
-    }
-  }
-  if(strcmp(flag, "cr")==0 || strcmp(flag, "")==0){
-    for(int i=0; i<3; i++){
-      print_CR(cr_table[i]);
-    }
-  }
-  return 0;
 }
+
+void lookup_CSG(LinkedList* list, CSG_RELATION csg){}
+void lookup_SNAP(LinkedList* list, SNAP_RELATION snap){}
+void lookup_CPQ(LinkedList* list, CPQ_RELATION cpq){}
+void lookup_CDH(LinkedList* list, CDH_RELATION cdh){}
+void lookup_CR(LinkedList* list, CR_RELATION cr){}
+
+void delete_CSG(LinkedList* list, CSG_RELATION csg){}
+void delete_SNAP(LinkedList* list, SNAP_RELATION snap){}
+void delete_CPQ(LinkedList* list, CPQ_RELATION cpq){}
+void delete_CDH(LinkedList* list, CDH_RELATION cdh){}
+void delete_CR(LinkedList* list, CR_RELATION cr){}
--- RelationalDataModel/rdm.h
+++ RelationalDataModel/rdm.h
@@ -1,68 +1,22 @@
 #ifndef RDM_H
 #define  RDM_H
+
 #include <string.h>
 #include <stdio.h>
 
-typedef struct _CSG* CSG_RELATION;
-struct _CSG {
-  char* course_name;
-  int student_id;
-  char* student_grade;
-};
-extern CSG_RELATION new_CSG(char* c, int s, char* g);
-extern void free_CSG();
-void print_CSG(CSG_RELATION csg){
-    printf("%s %d %s\n", csg->course_name, csg->student_id, csg->student_grade);
-}
+#include "LinkedList.h"
+#include "relations.h"
 
-typedef struct _SNAP* SNAP_RELATION;
-struct _SNAP  {
-  int student_id;
-  char* student_name;
-  char* student_address;
-  int student_phone;
-};
-extern SNAP_RELATION new_SNAP(int s, char* n, char* a, int p);
-extern void free_SNAP();
-void print_SNAP(SNAP_RELATION snap){
-  printf("%d %s %s %d\n",
-    snap->student_id, snap->student_name,
-    snap->student_address, snap->student_phone
-  );
-}
-
-typedef struct _CPQ* CPQ_RELATION;
-struct _CPQ {
-  char* course_name;
-  char* prereq_name;
-};
-extern CPQ_RELATION new_CPQ(char* c, char* pq);
-extern void free_CPQ();
-void print_CPQ(CPQ_RELATION cpq){
-  printf("%s %s\n", cpq->course_name, cpq->prereq_name);
-}
-
-typedef struct _CDH* CDH_RELATION;
-struct _CDH {
-  char* course_name;
-  char* course_day;
-  char* course_hour;
-};
-extern CDH_RELATION new_CDH(char* c, char* d, char* h);
-extern void free_CDH();
-void print_CDH(CDH_RELATION cdh){
-  printf("%s %s %s\n", cdh->course_name, cdh->course_day, cdh->course_hour);
-}
-
-typedef struct _CR* CR_RELATION;
-struct _CR {
-  char* course_name;
-  char* course_room;
-};
-extern CR_RELATION new_CR(char* c, char* r);
-extern void free_CR();
-void print_CR(CR_RELATION cr){
-  printf("%s %s\n", cr->course_name, cr->course_room);
-}
+extern void insert(LinkedList* list, void* data);
+extern void lookup_CSG(LinkedList* list, CSG_RELATION csg);
+extern void lookup_SNAP(LinkedList* list, SNAP_RELATION snap);
+extern void lookup_CPQ(LinkedList* list, CPQ_RELATION cpq);
+extern void lookup_CDH(LinkedList* list, CDH_RELATION cdh);
+extern void lookup_CR(LinkedList* list, CR_RELATION cr);
+extern void delete_CSG(LinkedList* list, CSG_RELATION csg);
+extern void delete_SNAP(LinkedList* list, SNAP_RELATION snap);
+extern void delete_CPQ(LinkedList* list, CPQ_RELATION cpq);
+extern void delete_CDH(LinkedList* list, CDH_RELATION cdh);
+extern void delete_CR(LinkedList* list, CR_RELATION cr);
 
 #endif
